3da6ad776934db39c43c58f9c28625c23c6b684c
DakotaRay13 <14drmanley@gmail.com>
2021-01-30 20:06:18 -0600

    Assignment Completed
    Required additions finished
    Video recorded

---
+++ GP_Assignment1/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    //Slider Variables
+    public Text sliderText;
+    public Slider slider;
+
+    //Input Field Variables
+    public InputField nameBox;
+    public Text nameText;
+
+    private void Start()
+    {
+        ShowSliderValue();
+        nameText.text = "";
+    }
+
+    public void Level01()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
+    public void Level02()
+    {
+        SceneManager.LoadScene("Level02");
+    }
+
+    public void Level03()
+    {
+        SceneManager.LoadScene("Level03");
+    }
+
+    public void ShowSliderValue()
+    {
+        sliderText.text = slider.value.ToString();
+    }
+
+    public void ThankYouPlayer()
+    {
+        nameText.text = "Thank you, " + nameBox.text + "!";
+    }
+}
--- GP_Assignment1/Assets/Scripts/Menu.cs
+++ GP_Assignment1/Assets/Scripts/Menu.cs
@@ -7,4 +7,14 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
